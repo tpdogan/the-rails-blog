2653ffcc4ad1171cd3c4689b14a6db5e7732b741
Tamer Dogan <pelintamerdogan@gmail.com>
2020-10-20 16:26:25 +0200

    Tags are connected among articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    list = []
+    tags.each {|tag| list.push(tag.name)}
+    list.join(", ")
+  end
+  def tag_list=(tags_string)
+    list = tags_string.split(",")
+    uniq_tag_array = list.map {|tag| tag.strip.downcase}.uniq
+
+    new_or_found = uniq_tag_array.map {|tag| Tag.find_or_create_by(name: tag)}
+    self.tags = new_or_found
+  end
 end
