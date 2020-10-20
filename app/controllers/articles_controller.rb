47e3972b3326da50a428ecfe9c0f34b60a98de39
Tamer Dogan <pelintamerdogan@gmail.com>
2020-10-20 12:00:38 +0200

    Destroy method is integrated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,38 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+=begin
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = "New article is successfully created."
+      format.html { redirect_to article_path(@article) }
+      format.json { render :show, status: :created, location: @article}
+    else
+      flash[:error] = "Not all fields are filled."
+      format.html { render :new }
+      format.json { render json: @article.errors, status: :unprocessable_entity}
+    end
+=end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
